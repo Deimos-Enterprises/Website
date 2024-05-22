56218f246b114a00981ac2fcb28dbe18adb1bfb5
Kaden Bautista <k07bautista@gmail.com>
2024-05-22 09:57:53 -0700

    gravity on earth and mars

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ const physicalConstants = {
     marsFriction: 0.1
 }
 
+const scalingConstants = {
+    gravityScale: 50
+}
+
 
 class TitleScene extends Phaser.Scene {
     constructor() {
@@ -77,25 +81,86 @@ class MarsScene extends Phaser.Scene {
     }
     preload() {
         this.load.image('mars-background', 'res/marsbackground.jpg');
+        this.load.image('earth-background', 'res/earthbackground.jpg');
         this.load.spritesheet('martian', 'res/martian.png', {frameWidth: 122, frameHeight: 158})
     }
     create() {
         console.log('Mars Scene')
-        this.add.image(0.5 * sizes.width, 0.5 * sizes.height, 'mars-background');
-        this.martian = this.createInteractable('martian', 122, 158, 1);
-    }
-    update() {}
+        this.earthBackground = this.add.image(0.5 * sizes.width, 0.5 * sizes.height, 'earth-background');
+        this.earthBackground.setScale(sizes.width / 313, sizes.height / 200);
+        this.marsBackground = this.add.image(0.5 * sizes.width, 0.5 * sizes.height, 'mars-background');
+        this.martian = this.createInteractable('martian', 122, 158, 1, [0, 1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
 
-    createInteractable(name, width, height, scale) {
+        this.isEarth = false;
+        this.keyE = this.input.keyboard.addKey('E');
+
+        this.timerText = this.add.text(sizes.width * 0.5, sizes.height * 0.5, '0:0', { font: '"Press Start 2P"' });
+        // this.timer = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
+        // this.timer
+    }
+    update() {
+        if(Phaser.Input.Keyboard.JustDown(this.keyE)) {
+            this.isEarth = !this.isEarth;
+        }
+
+        if(this.isEarth) {
+            this.earthBackground.setVisible(false);
+            this.marsBackground.setVisible(true);
+            this.martian.setGravityY(physicalConstants.marsGravity * scalingConstants.gravityScale);
+
+        } else {
+            this.marsBackground.setVisible(false);
+            this.earthBackground.setVisible(true);
+            this.martian.setGravityY(physicalConstants.earthGravity * scalingConstants.gravityScale);
+        }
+    }
+
+    formatTime(seconds){
+        // Minutes
+        var minutes = Math.floor(seconds/60);
+        // Seconds
+        var partInSeconds = seconds%60;
+        // Adds left zeros to seconds
+        partInSeconds = partInSeconds.toString().padStart(2,'0');
+        // Returns formated time
+        return `${minutes}:${partInSeconds}`;
+    }
+    
+
+    switchPlanets() {
+        if(this.isEarth) {
+            console.log('switch to earth');
+            this.background.setTexture('earth-background');
+        } else {
+            console.log('switch to mars');
+            this.background.setTexture('mars-background');
+        }
+    }
+
+    createInteractable(name, width, height, scale, moving, idle) {
         var temp = this.physics.add.sprite(width, height, name).setScale(scale, scale);
         temp.setActive(true);
         temp.velocityScaleX = 1;
         temp.velocityScaleYs = 1;
 
+        //animations
+        this.anims.create({
+            key: name + '-moving',
+            frames: this.anims.generateFrameNumbers(name, {frames: moving}),
+            frameRate: 8,
+            repeat: -1
+        });
+        this.anims.create({
+            key: name + '-idle',
+            frames: this.anims.generateFrameNumbers(name, {frames: idle}),
+            frameRate: 8,
+            repeat: -1
+        });
+
         // Mars physics
         temp.setBounce(0);
         temp.setFriction(0);
-        temp.setGravityY(physicalConstants.marsGravity);
+        temp.setGravityY(physicalConstants.earthGravity * scalingConstants.gravityScale);
         temp.setCollideWorldBounds(true);
 
         temp.setInteractive();
@@ -117,6 +182,15 @@ class MarsScene extends Phaser.Scene {
 
         return temp;
     } 
+}
+
+class EarthScene extends Phaser.Scene {
+    constructor() {
+        super('earth-scene');
+    }
+    preload() {}
+    create() {}
+    update() {}
 }
 
 const config = {
